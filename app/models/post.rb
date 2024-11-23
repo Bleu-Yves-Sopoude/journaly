0d4961e96e1483a0a5fc08cf9beabaf4e0e8414a
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2024-11-23 06:08:50 +0000

    methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  def count
+    postCounter
+  end
+
+  def rencent_post
+    post.comments.order(created_at: :desc).limit(5)
+  end
 end
